71e8b37be0b640c481460f98dbc9d7981d97c534
Fer-lando <fer.ram91@gmail.com>
2023-01-09 16:26:01 -0300

    carrito-compras

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const PORT = 3031
-const public = path.join(__dirname,"/public")
+const express=require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, ()=> console.log("servidor corriendo"))
+app.use(express.static("public"));
 
-app.use(express.static(public))
+app.listen(PORT,()=>console.log(`Servidor Corriendo en: ${PORT}`));
 
-app.get("/",(req,res)=>(res.sendFile(path.join(__dirname,"./views/productdetail.html"))))
-app.get("/login",(req,res)=>(res.sendFile(path.join(__dirname,"./views/login.html"))))
-app.get("/register",(req,res)=>(res.sendFile(path.join(__dirname,"./views/register.html"))))
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "./views/carrito.html"));
+});
+
+// app.get('/register', (req,res)=>{
+//     res.sendFile(__dirname + '/views/register.html');
+// });
+
+// app.get('/login', (req,res)=>{
+//     res.sendFile(__dirname + '/views/login.html');
+// });
+
+// app.get('/login', (req,res)=>{
+//     res.sendFile(__dirname + '/views/carrito.html');
+// });
+
