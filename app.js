fb888a290e70da4d96fa3f507b390b4bd420aec2
franSlepoy <slepoyfrancisco@gmail.com>
2023-01-18 17:43:44 -0300

    ahora si, estructura funcionando subiendo a git, dale campeon

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express=require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+app.use(express.static('public'));
 
-app.use(express.static("public"));
+const port = 3001;
+app.listen(port, ()=>console.log('Servidor corriendo en el puerto 3001'));
 
-app.listen(PORT,()=>console.log(`Servidor Corriendo en: ${PORT}`));
-
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname, "./views/carrito.html"));
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
 });
 
-// app.get('/register', (req,res)=>{
-//     res.sendFile(__dirname + '/views/register.html');
-// });
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
 
-// app.get('/login', (req,res)=>{
-//     res.sendFile(__dirname + '/views/login.html');
-// });
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
 
-// app.get('/login', (req,res)=>{
-//     res.sendFile(__dirname + '/views/carrito.html');
-// });
+app.get('/cart', (req,res)=>{
+    res.sendFile(__dirname + '/views/cart.html');
+}); 
+
+app.get('/productDetails', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetails.html');
+});
+
 
