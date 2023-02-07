4d085237d9d4104e8ab21b9551d27594da4f9e38
franSlepoy <slepoyfrancisco@gmail.com>
2023-02-07 14:40:09 -0300

    CRUD rutas

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 const productsControllers = require("../controllers/productsControllers");
 
+//mostrar productos
 router.get("/products", productsControllers.products);
+// formulario crear un producto
+router.get("/products/create", productsControllers.crate);
+//a donde se envia el producto
+router.post("/products", productsControllers.store);
+//Detalle de producto
+router.get("/products/:id/", productsControllers.detail);
+//editar un producto
+router.get("/products/:id/edit", productsControllers.edit);
+router.put("/:id", productsControllers.update);
+//Eliminar un producto
+router.delete("/products/:id", productsControllers.destroy);
+
 
 module.exports = router;
