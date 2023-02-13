c4872c01225f08241d78f78c8c27e56bf731ed74
CeciSy <ceciliasyrewicz@hotmail.com>
2023-02-12 21:57:56 -0300

    se corrige un error de merge

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -5,11 +5,7 @@ const mainControllers = require("../controllers/maincontrollers");
 router.get("/", mainControllers.index);
 router.get("/login", mainControllers.login);
 router.get("/register", mainControllers.register);
-<<<<<<< HEAD
-//router.get("/productdetails", mainControllers.productDetails);
-=======
 router.get("/productdetails", mainControllers.productDetails);
->>>>>>> 6f90a39a738ee748408f829d758f1cbf77f23f90
 router.get("/cart", mainControllers.cart);
 //router.get("/create", mainControllers.create);
 router.get("/edit", mainControllers.edit);
