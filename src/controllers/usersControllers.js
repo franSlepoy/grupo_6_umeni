926a8a73510ac9ed5c50cab2cbc7f72e31e1df8f
CeciSy <ceciliasyrewicz@hotmail.com>
2023-02-22 09:01:04 -0300

    se agrega multer y capacidad de subir archivo de perfil en el formulario de registro

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,6 +2,6 @@ const path = require("path");
 const usersControllers = {
     login: (req,res) => res.render(path.resolve(__dirname, "../views/users/login")),
     register: (req,res) => res.render(path.resolve(__dirname, "../views/users/register")),
-    processRegister: (req,res) => res.send("Procesando a traves de POST")
+    processRegister: (req,res) => res.send(req.body)
 }
 module.exports = usersControllers;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path")
+
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/images/avatars")
+    },
+    filename: (req, file, cb) => {
+        // let fileName=`${Date.now()}_img${path.extname(file.originalname)}`
+        // cb(null, filename)
+        let extArray = file.mimetype.split("/");
+        let extension = extArray[extArray.length - 1];
+        cb(null, file.fieldname + '-' + Date.now()+ '.' +extension)
+        }
+})
+
+const uploadFile = multer({ storage })
+
 const usersControllers = require("../controllers/usersControllers");
 
 router.get("/login", usersControllers.login);
 router.get("/register", usersControllers.register);
-router.post("/register", usersControllers.processRegister);
+router.post("/register", uploadFile.single("avatar"), usersControllers.processRegister);
 
 module.exports = router;
