1977df09ad1f1d5336ab1bc976fd49adcf0bd8f5
CeciSy <ceciliasyrewicz@hotmail.com>
2023-02-22 13:57:49 -0300

    avatres agregardos

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,6 +2,8 @@ const path = require("path");
 const usersControllers = {
     login: (req,res) => res.render(path.resolve(__dirname, "../views/users/login")),
     register: (req,res) => res.render(path.resolve(__dirname, "../views/users/register")),
-    processRegister: (req,res) => res.send(req.body)
+    processRegister: (req,res) => res.send({
+        body: req.body,
+        file: req.file})
 }
 module.exports = usersControllers;
