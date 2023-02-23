e5709c686a360d9f820f72011f57a602b74d2015
CeciSy <ceciliasyrewicz@hotmail.com>
2023-02-22 22:40:15 -0300

    se aceptan cambios al traer el repositorio

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,18 +1,25 @@
 const path = require("path");
 const { validationResult } = require("express-validator")
+
+const bcrypt = require("bcryptjs")
+
+let passEncriptada = bcrypt.hashSync("monito123", 10)
+let check = bcrypt.compareSync("monito123", passEncriptada)
+console.log(check)
+
 const usersControllers = {
     login: (req,res) => res.render(path.resolve(__dirname, "../views/users/login")),
     register: (req,res) => res.render(path.resolve(__dirname, "../views/users/register")),
     processRegister: (req,res) => {
         const resultValidation = validationResult(req)
-        res.send({
-            body: req.body,
-            file: req.file
-        })
-        //  if (resultValidation.errors.length > 0){
-        //  return res.render('register', {
-        //      errors: resultValidation.mapped(),
-        //  })
-    // }
+        //res.send({
+        //    body: req.body,
+        //    file: req.file
+        //})
+          if (resultValidation.errors.length > 0){
+          return res.render(path.resolve(__dirname, "../views/users/register"), {
+              errors: resultValidation.mapped(),
+          })
+     }
 }}
 module.exports = usersControllers;
