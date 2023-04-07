45f61c5e383aa9fe1b16321f3fb8a2b427e1a278
franSlepoy <slepoyfrancisco@gmail.com>
2023-04-07 18:48:18 -0300

    Maridaje.js en models

---
+++ src/database/models/Maridaje.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Maridaje"
+    let cols = {
+        idmaridaje:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        maridaje:{
+            type: dataTypes.STRING
+        }
+    }
+    let config = {
+        tableName : "maridaje",
+        timestamps : false
+    }
+    
+    const Maridaje = sequelize.define(alias, cols, config);
+
+    return Maridaje
+}
