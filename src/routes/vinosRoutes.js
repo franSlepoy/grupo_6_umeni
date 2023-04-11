5c15d52a9e0ab7b4f6404881e73a4da74a969505
franSlepoy <slepoyfrancisco@gmail.com>
2023-04-11 13:59:20 -0300

    listado de vinos y detalleVino funcionando y linkeados

--- src/routes/vinosRoutes.js
+++ src/routes/vinosRoutes.js
@@ -4,7 +4,7 @@ var vinosController = require("../controllers/vinosController");
 
 
 router.get("/", vinosController.list);
-router.get("/vinos/:id", vinosController.detail);
+router.get("/detalleVino/:id", vinosController.detail);
 
 router.get("/add", vinosController.add);
 router.post("/add", vinosController.create);
