53cc548a889ce318751a35a1d8da7b7447ce92a1
franSlepoy <slepoyfrancisco@gmail.com>
2023-04-21 11:48:11 -0300

    menu mobile

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mainControllers = require("../controllers/maincontrollers");
 
 router.get("/", mainControllers.index);
+router.get("/menuMobile", mainControllers.menuMobile);
+
 router.get("/productdetails", mainControllers.productDetails);
 router.get("/cart", mainControllers.cart);
 router.get("/edit", mainControllers.edit);
