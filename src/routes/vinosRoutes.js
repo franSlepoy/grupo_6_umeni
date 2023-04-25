160d166effe70e491d2a4c2e560221041fc2ad5d
franSlepoy <slepoyfrancisco@gmail.com>
2023-04-25 18:29:15 -0300

    configuracion de multer para productos terminada. Creacion de vinos con imagenes lista

--- src/routes/vinosRoutes.js
+++ src/routes/vinosRoutes.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "../../Public/images"));
+      cb(null, path.join(__dirname, "../../Public/images/productos"));
     },
     filename: function (req, file, cb) {
       cb(null, "imagen" + Date.now()+path.extname(file.originalname))
