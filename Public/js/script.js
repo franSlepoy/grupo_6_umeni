9c341e0a5b4a692ab8446f5b36db86fcc1229ba9
CeciSy <ceciliasyrewicz@hotmail.com>
2023-04-25 15:00:23 -0300

    se agrega validacion de extensión para la imagen de vino

--- Public/js/script.js
+++ Public/js/script.js
@@ -6,6 +6,8 @@ window.addEventListener("load", function(){
         let campoNombre = document.querySelector("input.name")
         console.log(campoNombre.value)
         let campoDescripcion = document.querySelector("input.descripcion")
+        let imagenVino = document.querySelector("input.imgVino")
+        console.log(imagenVino.value)
 
         if (campoNombre.value == ""){
             errores.push("El campo nombre de vino es obligatorio")
@@ -14,9 +16,15 @@ window.addEventListener("load", function(){
         }
         if (campoDescripcion.value == ""){
             errores.push("El campo descripción de vino es obligatorio")
-        } else if(campoNombre.value.length < 5){
-            errores.push("El campo descripción de vino debe tener al menos 5 caracteres")
+        } else if(campoDescripcion.value.length < 20){
+            errores.push("El campo descripción de vino debe tener al menos 20 caracteres")
         }
+        let fileName = imagenVino.value.split("\\").pop();
+        let allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;         
+        if (!allowedExtensions.exec(fileName)) {
+            errores.push("La imagen debe tener una extensión válida (.jpg, .jpeg o .png)")
+            }
+    
 
         if(errores.length > 0){
             e.preventDefault()
