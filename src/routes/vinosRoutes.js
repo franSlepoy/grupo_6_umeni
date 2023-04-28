c65befae34e47cc88948ffc51e14dcbfe8d9880f
franSlepoy <slepoyfrancisco@gmail.com>
2023-04-28 11:36:45 -0300

    validaciones del back en create

--- src/routes/vinosRoutes.js
+++ src/routes/vinosRoutes.js
@@ -4,6 +4,8 @@ const path = require("path");
 var vinosController = require("../controllers/vinosController");
 const multer = require("multer");
 
+const { body } = require("express-validator");
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, path.join(__dirname, "../../Public/images/productos"));
@@ -12,14 +14,26 @@ const storage = multer.diskStorage({
       cb(null, "imagen" + Date.now()+path.extname(file.originalname))
     }
   })
+const upload = multer({ storage: storage })
 
-  const upload = multer({ storage: storage })
+const validations = [
+  body("nombre").notEmpty().withMessage("Tenés que escribir un nombre de vino"),
+  body("descripcion").notEmpty().withMessage("Tenés que escribir una descripción"),
+  body("precio").notEmpty().withMessage("Tenés que escribir un precio"),
+  body("anio").notEmpty().withMessage("Tenés que escribir un año"),
+  body("bodega").notEmpty().withMessage("Tenés que seleccionar una bodega"),
+  body("linea").notEmpty().withMessage("Tenés que seleccionar una linea"),
+  body("cepa").notEmpty().withMessage("Tenés que seleccionar una cepa"),
+  body("maridaje").notEmpty().withMessage("Tenés que seleccionar un maridaje"),
+  body("potencialGuardado").notEmpty().withMessage("Tenés que escribir un potencial de guardado"),
+  body("volumen").notEmpty().withMessage("Tenés que escribir un volumen"),
+]
 
 router.get("/", vinosController.list);
 router.get("/detalleVino/:id", vinosController.detail);
 
 router.get("/add", vinosController.add);
-router.post("/add", upload.single("imagen"), vinosController.create);
+router.post("/add", upload.single("imagen"),validations, vinosController.create);
 
 router.get("/edit/:id", vinosController.edit);
 router.post("/edit/:id",upload.single("imagen"),vinosController.update );
