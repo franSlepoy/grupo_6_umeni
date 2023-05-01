62c2880027139f832eae4bdbe53cc4c48a507364
franSlepoy <slepoyfrancisco@gmail.com>
2023-04-30 22:07:31 -0300

    apiVinos terminada

--- src/controllers/apiVinos.js
+++ src/controllers/apiVinos.js
@@ -5,7 +5,22 @@ const Op = db.Sequelize.Op;
 module.exports = {
     list: async (req,res) => {
         const vinos = await db.Vino.findAll();
-        res.json(vinos)
+        res.json({
+            total: vinos.length ,
+            data: vinos ,
+            status: 200
+
+
+        })
      
+    },
+    show: async (req,res) => {
+        const vinos = await db.Vino.findByPk(req.params.id);
+        res.json({
+            
+            data: vinos,
+            status: 200
+        })
+
     }
 }
--- src/routes/apiVinosRoutes.js
+++ src/routes/apiVinosRoutes.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 const apiVinosController = require("../controllers/apiVinos");
 
-router.get("/", apiVinosController.list)
+router.get("/", apiVinosController.list);
+router.get("/detalle/:id", apiVinosController.show)
+
 
 module.exports = router
