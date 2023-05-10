5b89c8e98b94da33712b6fce6309b4ce72c11dc3
CeciSy <ceciliasyrewicz@hotmail.com>
2023-05-10 20:51:16 -0300

    cambios api vinos

--- src/controllers/apiVinos.js
+++ src/controllers/apiVinos.js
@@ -1,10 +1,13 @@
 //const { Op } = require("sequelize");
 const db = require("../database/models");
 const Op = db.Sequelize.Op;
+const { QueryTypes } = require('sequelize');
 
 module.exports = {
     list: async (req,res) => {
         const vinos = await db.Vino.findAll();
+        const cepas = await db.Cepa.findAll();
+        const vinosFiltro = await Sequelize.Op.query("SELECT vino, COUNT (*) AS vinoPorCepa FROM `vinos` GROUP BY `cepas`", { type: QueryTypes.SELECT });
         res.json({
             total: vinos.length ,
             data: vinos ,
