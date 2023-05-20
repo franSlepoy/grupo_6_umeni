49364c940ba685c9a8d5a434bda844bc710f626e
Fer-lando <fer.ram91@gmail.com>
2023-05-20 10:09:34 -0300

    detalle carrito

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const mainControllers = require("../controllers/maincontrollers");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", mainControllers.index);
 router.get("/menuMobile", mainControllers.menuMobile);
-router.get("/cart", mainControllers.cart);
+router.get("/cart", authMiddleware, mainControllers.cart);
 
 
 
